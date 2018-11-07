98bbc50d94745b1623bfa1d8e9b2d4993e716725
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-07 11:19:15 +0100

    Update gamma.div.r

--- gamma.div.r
+++ gamma.div.r
@@ -1,6 +1,6 @@
 gamma.div <- function(x,w,q){
 if (q==1) {q=0.99999}
-x <- x[apply(x, 1, function(z) !all(z==0)),]
+x <- x[apply(x, 1, function(z) !all(z==0)),] # 20181107 - new addition to remove all-zero rows
 pi.w <- sweep(x,2,w,"*")
 div <- sum(rowSums(pi.w)^q)^(1/(1-q))
 return(div)

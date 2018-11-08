d93ed0bde0232c5d67280a786e0c7ea8ebbe4733
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-08 14:24:18 +0100

    Update div.part.r

--- R/div.part.r
+++ R/div.part.r
@@ -9,13 +9,10 @@ if(missing(qvalue)) stop("q value is missing")
 if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
 if (qvalue==1) {qvalue=0.99999}
 if(missing(hierarchy)) warning("Assuming a two-level hierarchy: 1) sample, 2) total dataset")
-if(is.null(dim(hierarchy)) == TRUE) stop("The hierarchy table is not a matrix")
-if(dim(hierarchy)[2] < 2) stop("The hierarchy table contains less than 2 columns")
-if(missing(hierarchy)) { hierarchy= c()}
 if(missing(weight)) { weight= rep(1/ncol(otutable),ncol(otutable))}
 
 #Function for 2-level hierarchy
-if(is.null(hierarchy) == TRUE){
+if(missing(hierarchy)){
 alpha <- DiverHill::alpha.div(otutable,qvalue)
 gamma <- DiverHill::gamma.div(otutable,qvalue)
 beta <- gamma/alpha

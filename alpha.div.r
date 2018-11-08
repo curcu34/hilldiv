d64edd789c8b34b621ed0af4bbbbb1f9e6bb67e2
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-08 11:37:49 +0100

    Update alpha.div.r

--- alpha.div.r
+++ alpha.div.r
@@ -1,8 +1,9 @@
 alpha.div <- function(x,w,q){
 if (q==1) {q=0.99999}
-pi.w <- sweep(x,2,w,"*")
+pi <- as.data.frame(x[apply(x, 1, function(z) !all(z==0)),])
+pi.w <- sweep(pi,2,w,"*")
 pi.w.q <- pi.w^q
-pi.w.q[!x] <- 0
+pi.w.q[!pi] <- 0
 N <- length(w)
 div <- sum(rowSums(pi.w.q))^(1/(1-q))/N
 return(div)

4ddc5d1bc7c077f5537c1fee22a762f319b97c4e
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-08 13:16:43 +0100

    Update true.div.r

--- R/true.div.r
+++ R/true.div.r
@@ -1,6 +1,14 @@
-true.div <- function(x,q){ 
-if(q==1){q=0.99999} 
-pi <- x[x!=0] 
-div <- sum(pi^q)^(1/(1-q))
+true.div <- function(vector,qvalue){ 
+  
+#Quality-check and warnings
+if(missing(vector)) stop("Vector is missing")
+if(sum(vector) != 1) stop("The vector does not sum up to 1")
+if(missing(qvalue)) stop("q value is missing")
+if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
+  
+#Function
+if(qvalue==1){qvalue=0.99999} 
+pi <- vector[vector!=0] 
+div <- sum(pi^qvalue)^(1/(1-qvalue))
 return(div) 
 }

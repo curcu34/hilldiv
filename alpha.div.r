d46937dcec60399bdf53c7f4c9dde313f20cf70a
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-08 11:40:35 +0100

    Update alpha.div.r

--- alpha.div.r
+++ alpha.div.r
@@ -1,10 +1,10 @@
 alpha.div <- function(x,w,q){
-if (q==1) {q=0.99999}
-pi <- as.data.frame(x[apply(x, 1, function(z) !all(z==0)),])
-pi.w <- sweep(pi,2,w,"*")
-pi.w.q <- pi.w^q
-pi.w.q[!pi] <- 0
-N <- length(w)
-div <- sum(rowSums(pi.w.q))^(1/(1-q))/N
-return(div)
+if (q==1) {q=0.99999} # change q to the limit of the unity (0.99999) if q=1
+pi <- as.data.frame(x[apply(x, 1, function(z) !all(z==0)),]) #remove OTUs without abundances (=all-zero rows) 
+pi.w <- sweep(pi,2,w,"*") #apply weights 
+pi.w.q <- pi.w^q #apply order of diversity 
+pi.w.q[!pi] <- 0 #only necessary when q=0, to turn 1s created when making the exponentials of 0 to 0  
+N <- length(w) #calculate number of samples
+div <- sum(rowSums(pi.w.q))^(1/(1-q))/N #apply alpha diversity formula
+return(div) #print the result
 }

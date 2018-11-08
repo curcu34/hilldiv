4e13d0ead204327140fb41a0f82174a17f08e63d
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-08 11:50:53 +0100

    Update gamma.div.r

--- gamma.div.r
+++ gamma.div.r
@@ -1,7 +1,7 @@
 gamma.div <- function(x,w,q){
-if (q==1) {q=0.99999}
-x <- x[apply(x, 1, function(z) !all(z==0)),] # 20181107 - new addition to remove all-zero rows
-pi.w <- sweep(x,2,w,"*")
-div <- sum(rowSums(pi.w)^q)^(1/(1-q))
-return(div)
+if (q==1) {q=0.99999} # change q to the limit of the unity (0.99999) if q=1
+pi <- as.data.frame(x[apply(x, 1, function(z) !all(z==0)),]) #remove OTUs without abundances (=all-zero rows) 
+pi.w <- sweep(pi,2,w,"*") #apply weights 
+div <- sum(rowSums(pi.w)^q)^(1/(1-q)) #apply alpha diversity formula
+return(div) #print the result
 }

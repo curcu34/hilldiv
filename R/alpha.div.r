ed4ed2603043880260e7bf4fc662c4bcbe15c3a4
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-08 12:53:17 +0100

    Update alpha.div.r

--- R/alpha.div.r
+++ R/alpha.div.r
@@ -1,6 +1,6 @@
 alpha.div <- function(otutable,qvalue,weight){
     
-#Quality-check
+#Quality-check and warnings
 if(missing(otutable)) stop("OTU table is missing")    
 if(dim(otutable)[1] < 2) stop("The OTU table only less than 2 OTUs")
 if(dim(otutable)[2] < 2) stop("The OTU table contains less than 2 samples")

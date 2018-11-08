8a25f1c06fcf0d3ad0a2d4da3092f9e7d6c31262
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-08 13:12:59 +0100

    Update gamma.phylodiv.r

--- R/gamma.phylodiv.r
+++ R/gamma.phylodiv.r
@@ -1,16 +1,30 @@
-gamma.phylodiv <- function(x,w,t,q){
-if (q==1) {q=0.99999}
-x <- as.data.frame(x)
-wj <- w
-N <- ncol(x)
-Li <- t$edge.length
-ltips <- sapply(t$edge[, 2], function(node) geiger::tips(t, node))
-aij <- matrix(unlist(lapply(ltips, function(TipVector) colSums(x[TipVector,]))), ncol = N, byrow = TRUE)
+gamma.phylodiv <- function(otutable,qvalue,tree,weight){
+ 
+  #Quality-check and warnings
+if(missing(otutable)) stop("OTU table is missing")
+if(is.null(dim(otutable)) == TRUE) stop("The OTU table is not a matrix")
+if(dim(otutable)[1] < 2) stop("The OTU table only less than 2 OTUs")
+if(dim(otutable)[2] < 2) stop("The OTU table contains less than 2 samples")
+if(missing(qvalue)) stop("q value is missing")
+if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
+if(missing(tree)) stop("Tree is missing")
+if(identical(sort(rownames(otutable)),sort(tree$tip.label)) == FALSE) stop("OTU names in the OTU table and tree do not match")
+if(missing(weight)) { weight= rep(1/ncol(otutable),ncol(otutable))}
+if(missing(weight)) warning("Assuming equal weights")
+  
+#Function
+if (qvalue==1) {qvalue=0.99999}
+x <- as.data.frame(otutable)
+wj <- weight
+N <- ncol(otutable)
+Li <- tree$edge.length
+ltips <- sapply(tree$edge[, 2], function(node) geiger::tips(tree, node))
+aij <- matrix(unlist(lapply(ltips, function(TipVector) colSums(otutable[TipVector,]))), ncol = N, byrow = TRUE)
 aij.wj <- sweep(aij, 2, wj, "*")
 ai <- rowSums(aij.wj)
 T <- sum(sweep(aij.wj, 1, Li, "*"))
 L <- matrix(rep(Li, N), ncol = N)
 wm <-  matrix(rep(wj, length(Li)), ncol = N, byrow=TRUE)
-phylodiv <- sum(Li * (ai/T)^q)^(1/(1 - q))
+phylodiv <- sum(Li * (ai/T)^qvalue)^(1/(1 - qvalue))
 return(phylodiv)
 }

26bec1ade990f7d241fe5b396b9057a3dd6c0802
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-09 13:33:37 +0100

    Update pair.dis.r

--- R/pair.dis.r
+++ R/pair.dis.r
@@ -11,9 +11,6 @@ if (qvalue==1) {qvalue=0.99999}
 if(missing(hierarchy)) warning("Assuming no hierarchy")
 if(missing(measure)) { measure= c("homogeneity","overlap","turnover")}
 
-##Function for no hierarchy
-if(missing(hierarchy)){
-
 #Create matrices
 L1 <- sort(colnames(otutable))
 N <- length(L1)
@@ -68,12 +65,83 @@ results[["Turnover"]] <- L1_turnover}
 }
 }
 
-return(results)
+#If hierarchy specified, also do:
+if(ncol(hierarchy) == 2){
+
+#Check hierarchy file
+hierarchy[,1] <- as.character(hierarchy[,1])
+hierarchy[,2] <- as.character(hierarchy[,2])
+if(identical(sort(colnames(otutable)),sort(hierarchy[,1])) == FALSE) stop("OTU names in the OTU table and the hierarchy table do not match")
+colnames(hierarchy) <- c("L1","L2")
+
+#Prepare L2 OTU table
+otutable.L2 <- merge(t(otutable),hierarchy, by.x="row.names",by.y="L1")
+rownames(otutable.L2) <- otutable.L2[,1]
+otutable.L2 <- otutable.L2[,-1]
+otutable.L2 <- aggregate(otutable.L2[,-ncol(otutable.L2)], by=list(otutable.L2[,ncol(otutable.L2)]), FUN=sum)
+rownames(otutable.L2) <- otutable.L2[,1]
+otutable.L2 <- otutable.L2[,-1]
+otutable.L2 <- t(sweep(otutable.L2, 1, rowSums(otutable.L2), FUN="/"))
+
+#Create L2 matrices
+L2 <- sort(colnames(otutable.L2))
+N <- length(L2)
+
+L2_beta <- matrix(rep(NA,length(L2)^2), nrow = length(L2), ncol = length(L2))
+colnames(L2_beta) <- L2
+rownames(L2_beta) <- L2
+
+if('homogeneity' %in% measure){
+L2_homogeneity <- matrix(rep(NA,length(L2)^2), nrow = length(L2), ncol = length(L2))
+colnames(L2_homogeneity) <- L2
+rownames(L2_homogeneity) <- L2
 }
 
-#Function for 2-level hierarchy
+if('overlap' %in% measure){
+L2_overlap <- matrix(rep(NA,length(L2)^2), nrow = length(L2), ncol = length(L2))
+colnames(L2_overlap) <- L2
+rownames(L2_overlap) <- L2
+}
 
+if('turnover' %in% measure){
+L2_turnover <- matrix(rep(NA,length(L2)^2), nrow = length(L2), ncol = length(L2))
+colnames(L2_turnover) <- L2
+rownames(L2_turnover) <- L2
+}
+
+for (x in L2){
+for (y in L2){
+if(is.na(L2_beta[y,x])){ #to avoid repeating mirror operations
+combination <- otutable.L2[,c(x,y)]
+alpha <- alpha.div(combination,qvalue)
+gamma <- gamma.div(combination,qvalue)
+beta <- gamma/alpha
+L2_beta[x,y] <- beta
+results <- list("Beta_L2" = L2_beta)
+
+if('homogeneity' %in% measure){
+homogeneity <- ((1/beta) - 1/N)/(1-1/N)
+L2_homogeneity[x,y] <- homogeneity
+results[["Homogeneity_L2"]] <- L2_homogeneity}
+
+if('overlap' %in% measure){
+overlap <-((1/beta)^(1-qvalue) - (1/N)^(1-qvalue)) / (1 - (1/N)^(1-qvalue))
+L2_overlap[x,y] <- overlap
+results[["Overlap_L2"]] <- L2_overlap}
+
+if('turnover' %in% measure){
+turnover <- (beta - 1)/(N-1)
+L2_turnover[x,y] <- turnover
+results[["Turnover_L2"]] <- L2_turnover}
+}
+}
+}
   
+  
+}
+  
+return(results)
+
 }
 
 

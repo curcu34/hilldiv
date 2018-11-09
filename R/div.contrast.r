d7ad8c2e7f7333f4a9b243a960c6f7fe8962dab4
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-09 17:47:58 +0100

    Update div.contrast.r

--- R/div.contrast.r
+++ R/div.contrast.r
@@ -1,4 +1,4 @@
-div.contrast <- function(otutable,qvalue,hierarchy){ 
+div.contrast <- function(otutable,qvalue,hierarchy,chart){ 
 if(missing(otutable)) stop("OTU table is missing")
 if(is.null(dim(otutable)) == TRUE) stop("The OTU table is not a matrix")
 if(dim(otutable)[1] < 2) stop("The OTU table only less than 2 OTUs")
@@ -6,17 +6,28 @@ if(dim(otutable)[2] < 2) stop("The OTU table contains less than 2 samples")
 if(missing(qvalue)) stop("q value is missing")
 if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
 if(missing(hierarchy)) stop("Hierarchy table is necessary to contrast groups of samples")
+if(missing(chart)) {chart="boxplot"}
 
 div.values <- true.div(otu.table.norm,qvalue)
 div.values.groups <- merge(t(t(div.values)),hierarchy,by.x="row.names",by.y="Sample")
 colnames(div.values.groups) <- c("Sample","Value","Group")
 
 #Plot
+if(chart == "boxplot"){
 getPalette = colorRampPalette(brewer.pal(length(unique(div.values.groups$Group)), "Paired"))
 plot <- ggplot(div.values.groups, aes(x=Group, y=Value, colour=Group)) + 
   geom_boxplot() +
   coord_flip()
 print(plot)
+}
+if(chart == "jitter"){
+getPalette = colorRampPalette(brewer.pal(length(unique(div.values.groups$Group)), "Paired"))
+plot <- ggplot(div.values.groups, aes(x=Group, y=Value, colour=Group)) + 
+  geom_jitter(width = 0.1) +
+  coord_flip()
+print(plot)
+}  
+  
 
 #Statistical test
 if(length(unique(div.values.groups$Group)) == 2){

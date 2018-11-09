11c6bcfd71531ae74b5c2b1f5929ed9b1c9a82bf
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-09 14:41:23 +0100

    Update div.profile.r

--- R/div.profile.r
+++ R/div.profile.r
@@ -8,26 +8,30 @@ if(missing(order)) { order= seq(from = 0, to = 5, by = (0.1))}
 
 #If input data is a vector
 if(is.null(dim(abund)) == TRUE){
-
-profile <- c()
-for (o in order){
-div.value <- true.div(abund,o)
-profile <- c(profile,div.value)
-}
-profile.melted <- as.data.frame(cbind(order,profile))
-ggplot(profile.melted , aes(x = order, y = profile)) +
-       geom_line() + 
-       xlab("Order of diversity") + ylab("Effective number of OTUs") +
-       theme_minimal()
-  
-}
-
+    profile <- c()
+    for (o in order){
+    div.value <- true.div(abund,o)
+    profile <- c(profile,div.value)
+    }
+    profile.melted <- as.data.frame(cbind(order,profile))
+    ggplot(profile.melted , aes(x = order, y = profile)) +
+           geom_line() + 
+           xlab("Order of diversity") + ylab("Effective number of OTUs") +
+           theme_minimal()
 }
   
 #If input data is an OTU table
 if(is.null(dim(abund)) == FALSE){
-if(dim(otutable)[1] < 2) stop("The OTU table only less than 2 OTUs")
-if(dim(otutable)[2] < 2) stop("The OTU table contains less than 2 samples")
+    
+    if(dim(otutable)[1] < 2) stop("The OTU table only less than 2 OTUs")
+    if(dim(otutable)[2] < 2) stop("The OTU table contains less than 2 samples")
+    
+    profile <- c()
+    for (o in order){
+    div.value <- true.div(abund,o)
+    profile <- c(profile,div.value)
+    }    
+    
+}
 
 }
-  

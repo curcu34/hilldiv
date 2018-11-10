c100c90004aff8baaabbcbdbf227678ff5785b9a
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-10 12:46:59 +0100

    Update div.comp.test.r

--- R/div.comp.test.r
+++ R/div.comp.test.r
@@ -1,4 +1,4 @@
-div.comp.test <- function(otutable,qvalue,hierarchy){ 
+div.comp.test <- function(otutable,qvalue,hierarchy,tree){ 
 if(missing(otutable)) stop("OTU table is missing")
 if(is.null(dim(otutable)) == TRUE) stop("The OTU table is not a matrix")
 if(dim(otutable)[1] < 2) stop("The OTU table only less than 2 OTUs")
@@ -6,8 +6,14 @@ if(dim(otutable)[2] < 2) stop("The OTU table contains less than 2 samples")
 if(missing(qvalue)) stop("q value is missing")
 if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
 if(missing(hierarchy)) stop("Hierarchy table is necessary to contrast groups of samples")
+if(missing(tree)) stop("Tree is missing")
+if(identical(sort(rownames(otutable)),sort(tree$tip.label)) == FALSE) stop("OTU names in the OTU table and tree do not match")
 
+if(missing(tree)){
 div.values <- true.div(otutable,qvalue)
+}else{
+div.values <- true.div(otutable,qvalue,tree)
+}
 div.values.groups <- merge(t(t(div.values)),hierarchy,by.x="row.names",by.y="Sample")
 colnames(div.values.groups) <- c("Sample","Value","Group")
 

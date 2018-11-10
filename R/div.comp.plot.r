93f055323cf20c39f3d8ecc207c2893f098e9a12
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-10 14:12:11 +0100

    Update div.comp.plot.r

--- R/div.comp.plot.r
+++ R/div.comp.plot.r
@@ -1,4 +1,4 @@
-div.comp.plot <- function(otutable,qvalue,hierarchy,chart){ 
+div.comp.plot <- function(otutable,qvalue,hierarchy,tree,chart){ 
 if(missing(otutable)) stop("OTU table is missing")
 if(is.null(dim(otutable)) == TRUE) stop("The OTU table is not a matrix")
 if(dim(otutable)[1] < 2) stop("The OTU table only less than 2 OTUs")
@@ -8,7 +8,13 @@ if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
 if(missing(hierarchy)) stop("Hierarchy table is necessary to contrast groups of samples")
 if(missing(chart)) {chart="boxplot"}
 
+if(missing(tree)){
 div.values <- true.div(otutable,qvalue)
+}else{
+if(class(tree) != "phylo") stop("Tree needs to be an object of class Phylo")  
+if(identical(sort(rownames(otutable)),sort(tree$tip.label)) == FALSE) stop("OTU names in the OTU table and tree do not match")
+div.values <- true.div(otutable,qvalue,tree)
+}
 div.values.groups <- merge(t(t(div.values)),hierarchy,by.x="row.names",by.y="Sample")
 colnames(div.values.groups) <- c("Sample","Value","Group")
 

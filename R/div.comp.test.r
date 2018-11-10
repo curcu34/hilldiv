2f8292b579fae040729785e1bda79dbbd64cf69a
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-10 17:40:42 +0100

    Update div.comp.test.r

--- R/div.comp.test.r
+++ R/div.comp.test.r
@@ -8,11 +8,11 @@ if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
 if(missing(hierarchy)) stop("Hierarchy table is necessary to contrast groups of samples")
 
 if(missing(tree)){
-div.values <- true.div(otutable,qvalue)
+div.values <- hill.div(otutable,qvalue)
 }else{
 if(class(tree) != "phylo") stop("Tree needs to be an object of class Phylo")  
 if(identical(sort(rownames(otutable)),sort(tree$tip.label)) == FALSE) stop("OTU names in the OTU table and tree do not match")
-div.values <- true.div(otutable,qvalue,tree)
+div.values <- hill.div(otutable,qvalue,tree)
 }
 div.values.groups <- merge(t(t(div.values)),hierarchy,by.x="row.names",by.y="Sample")
 colnames(div.values.groups) <- c("Sample","Value","Group")

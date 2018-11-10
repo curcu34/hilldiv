a1ba539648544a9b69d91ef372fbf9cea4d61e82
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-10 18:24:03 +0100

    Update gamma.div.r

--- R/gamma.div.r
+++ R/gamma.div.r
@@ -1,4 +1,4 @@
-gamma.div <- function(otutable,qvalue,weight){
+gamma.div <- function(otutable,qvalue,tree,weight){
 
 #Quality-check and warnings
 if(missing(otutable)) stop("OTU table is missing")
@@ -9,10 +9,29 @@ if(missing(qvalue)) stop("q value is missing")
 if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
 if(missing(weight)) { weight= rep(1/ncol(otutable),ncol(otutable))}
 if(missing(weight)) warning("Assuming equal weights")
-  
-if (qvalue==1) {qvalue=0.99999} # change q to the limit of the unity (0.99999) if q=1
-pi <- as.data.frame(otutable[apply(otutable, 1, function(z) !all(z==0)),]) #remove OTUs without abundances (=all-zero rows) 
-pi.w <- sweep(pi,2,weight,"*") #apply weights 
-div <- sum(rowSums(pi.w)^qvalue)^(1/(1-qvalue)) #apply alpha diversity formula
-return(div) #print the result
+
+if(missing(tree)){
+    #Neutral
+    if (qvalue==1) {qvalue=0.99999} # change q to the limit of the unity (0.99999) if q=1
+    pi <- as.data.frame(otutable[apply(otutable, 1, function(z) !all(z==0)),]) #remove OTUs without abundances (=all-zero rows) 
+    pi.w <- sweep(pi,2,weight,"*") #apply weights 
+    div <- sum(rowSums(pi.w)^qvalue)^(1/(1-qvalue)) #apply alpha diversity formula
+    return(div) #print the result
+}else{
+    #Non-neutral
+    if(identical(sort(rownames(otutable)),sort(tree$tip.label)) == FALSE) stop("OTU names in the OTU table and tree do not match")
+    otutable <- as.data.frame(otutable)
+    wj <- weight
+    N <- ncol(otutable)
+    Li <- tree$edge.length
+    ltips <- sapply(tree$edge[, 2], function(node) geiger::tips(tree, node))
+    aij <- matrix(unlist(lapply(ltips, function(TipVector) colSums(otutable[TipVector,]))), ncol = N, byrow = TRUE)
+    aij.wj <- sweep(aij, 2, wj, "*")
+    ai <- rowSums(aij.wj)
+    T <- sum(sweep(aij.wj, 1, Li, "*"))
+    L <- matrix(rep(Li, N), ncol = N)
+    wm <-  matrix(rep(wj, length(Li)), ncol = N, byrow=TRUE)
+    phylodiv <- sum(Li * (ai/T)^qvalue)^(1/(1 - qvalue))
+    return(phylodiv)
+}
 }

b6b5ae1caa0d0d7ea2840c53dd7a88684856e094
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-14 13:19:48 +0100

    Update div.profile.r

--- R/div.profile.r
+++ R/div.profile.r
@@ -2,9 +2,9 @@ div.profile <- function(abund,tree,order,values,hierarchy){
     
 #Quality-check and warnings
 if(missing(abund)) stop("The abundance data is missing")
-if(missing(order)) { order= seq(from = 0, to = 5, by = (0.1))}
-if(missing(values)) { values= "FALSE"}
-
+if(missing(order)) {order= seq(from = 0, to = 5, by = (0.1))}
+if(missing(values)) {values= "FALSE"}
+    
 #If input data is a vector
 if(is.null(dim(abund)) == TRUE){
     profile <- c()
@@ -50,9 +50,18 @@ if(is.null(dim(abund)) == FALSE){
     if(hasArg(hierarchy) == TRUE){
     colnames(hierarchy) <- c("Sample","Group")
     profile.melted2 <- merge(profile.melted,hierarchy,by="Sample")
-    profile.melted_mean <- aggregate(profile.melted2[,"Value"],by=list(profile.melted2[,"Group"],FUN=mean)
-    profile.melted_sterr <- aggregate(profile.melted2[,"Value"],by=list(profile.melted2[,"Group"],FUN=sd)#change for standard error
-    #add plot code with mean+stderr
+    profile.melted_mean <- aggregate(profile.melted2[,"Value"],by=list(profile.melted2[,c("Group")],profile.melted2[,c("Order")]),FUN=mean)
+    std.err <- function(x) sd(x)/sqrt(length(x))
+    profile.melted_sterr <- aggregate(profile.melted2[,"Value"],by=list(profile.melted2[,c("Group")],profile.melted2[,c("Order")]),FUN=std.err)
+    profile.melted <- cbind(profile.melted_mean,profile.melted_sterr[,3])
+    colnames(profile.melted) <- c("Group","Order","Mean","Stderr")
+      plot <- ggplot(profile.melted , aes(x = Order, y = Mean, group=Group, colour=Group)) +
+        geom_line() + 
+        geom_ribbon(aes(ymin = Mean - Stderr, ymax = Mean + Stderr, fill=Group), alpha = 0.2, colour=NA) +
+        xlab("Order of diversity") + ylab("Effective number of OTUs") +
+        scale_fill_manual(values = getPalette(ncol(abund))) + 
+        scale_colour_manual(values = getPalette(ncol(abund))) + 
+        theme_minimal()
     }else{    
     plot <- ggplot(profile.melted , aes(x = Order, y = Value, group=Sample, colour=Sample)) +
         geom_line() + 

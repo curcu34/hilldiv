f43746874ea62f0264bbccdbb320967310cc42f1
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-19 11:51:48 +0100

    Update hill.dis.r

--- R/hill.dis.r
+++ R/hill.dis.r
@@ -10,7 +10,7 @@ if (missing(type)) {type="dissimilarity"}
 
 #Sørensen-type species-overlap (CqN, 1-CqN)
 if (metric == "C"){
-CqN <- ((1/beta)^(1-qvalue) - (1/N)^(1-qvalue)) / (1 - (1/N)^(1-qvalue))
+CqN <- ((1/beta)^(qvalue-1) - (1/N)^(qvalue-1)) / (1 - (1/N)^(qvalue-1))
 return(CqN)
 if (type == "dissimilarity"){
 rCqN <- 1 - CqN
@@ -20,7 +20,12 @@ return(rCqN)
 
 #Jaccard-type species-overlap (UqN, 1-UqN)
 if (metric == "U"){
-if (type == "dissimilarity"){ }
+UqN <- ((1/beta)^(1-qvalue) - (1/N)^(1-qvalue)) / (1 - (1/N)^(1-qvalue))
+return(UqN)
+if (type == "dissimilarity"){
+rUqN <- 1 - UqN
+return(rUqN)
+}
 }
 
 #Sørensen-type turnover (VqN, 1-VqN)

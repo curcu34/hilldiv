6320920d94506ddb3b6a888b57dcbd86813784e9
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-19 15:00:29 +0100

    Update beta.dis.r

--- R/beta.dis.r
+++ R/beta.dis.r
@@ -11,41 +11,41 @@ if (missing(type)) {type="dissimilarity"}
 #Sørensen-type overlap (CqN, 1-CqN)
 if (metric == "C"){
 CqN <- ((1/beta)^(qvalue-1) - (1/N)^(qvalue-1)) / (1 - (1/N)^(qvalue-1))
+  if (type == "dissimilarity"){
+  rCqN <- 1 - CqN
+  return(rCqN)
+  }
 return(CqN)
-if (type == "dissimilarity"){
-rCqN <- 1 - CqN
-return(rCqN)
-}
 }
 
 #Jaccard-type overlap (UqN, 1-UqN)
 if (metric == "U"){
 UqN <- ((1/beta)^(1-qvalue) - (1/N)^(1-qvalue)) / (1 - (1/N)^(1-qvalue))
+  if (type == "dissimilarity"){
+  rUqN <- 1 - UqN
+  return(rUqN)
+  }
 return(UqN)
-if (type == "dissimilarity"){
-rUqN <- 1 - UqN
-return(rUqN)
-}
 }
 
 #Sørensen-type turnover (VqN, 1-VqN)
 if (metric == "V"){
 VqN <-  (beta - 1)/(N-1)
+  if (type == "similarity"){ 
+  rVqN <- 1 - VqN
+  return(rVqN)
+  }
 return(VqN)
-if (type == "similarity"){ 
-rVqN <- 1 - VqN
-return(rVqN)
-}
 } 
   
 #Jaccard-type turnover (SqN, 1-SqN)
 if (metric == "S"){
 SqN <- ((1/beta) - 1/N)/(1-1/N)
+  if (type == "dissimilarity"){
+  rSqN <- 1 - SqN
+  return(rSqN)
+  }
 return(SqN)
-if (type == "dissimilarity"){
-rSqN <- 1 - SqN
-return(rSqN)
-}
 } 
   
 }

04711e8aa75df2b357756e6732b3f78c2c4d4708
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-21 10:54:59 +0100

    Create gamma.curve.r

---
+++ R/gamma.curve.r
@@ -0,0 +1,13 @@
+gamma.curve <- function(otutable,qvalue,steps,iter){
+
+matrix <- c()
+for (i in 1:iter){#open iteration
+vector <- c()
+  for (i in 1:ncol(otu.table.norm)){#open steps
+
+
+  }#close steps
+
+}#close iteration
+
+}#close function

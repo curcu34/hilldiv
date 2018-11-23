e05659221efdaf1414bb443bade0ff3ed76069ae
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-11-23 05:56:00 +0100

    Update gamma.div.r

--- R/gamma.div.r
+++ R/gamma.div.r
@@ -5,6 +5,7 @@ if(missing(otutable)) stop("OTU table is missing")
 if(is.null(dim(otutable)) == TRUE) stop("The OTU table is not a matrix")
 if(dim(otutable)[1] < 2) stop("The OTU table only less than 2 OTUs")
 if(dim(otutable)[2] < 2) stop("The OTU table contains less than 2 samples")
+if(sum(colSums(otutable)) != ncol(otutable)) {otutable <- tss(otutable)}
 if(missing(qvalue)) stop("q value is missing")
 if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
 if (qvalue==1) {qvalue=0.99999} # change q to the limit of the unity (0.99999) if q=1

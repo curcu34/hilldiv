6ca82d4b691ff070939111093f413484206598c1
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-12-13 19:31:10 +0100

    Update hill.intext.r

--- R/hill.intext.r
+++ R/hill.intext.r
@@ -1,7 +1,7 @@
 hill.intext <- function(otutable,qvalue,hierarchy,tree){
 
 #qvalue can only be 0, 1 or 2
-#Need to specify observed value
+#Also add completeness plot
 
 #Generate lists
 if(!missing(hierarchy)){
@@ -12,13 +12,11 @@ lists <- to.inext(otutable)
 maxsize <- ncol(otutable)
 }
 
-#Run iNEXT/iNextPD
+#NEUTRAL DIVERSITY
 if(!missing(tree)){
+  
+#Run iNEXT
 sp.inext <- iNEXT(lists, q=qvalue, datatype="incidence_freq",size=seq(1,maxsize*3,round(maxsize*3/20)))
-}else{
-if(class(tree) != "phylog"){tree.phylog <- phylo.to.phylog(tree)}
-sp.inext <- iNextPD(lists, q=qvalue, datatype="incidence_freq",phy=tree.phylog,size=seq(1,maxsize*3,round(maxsize*3/20)))
-}
 
 #Extract data from iNEXT object
 table <- c()
@@ -46,5 +44,38 @@ scale_colour_manual(values = getPalette(length(lists))) +
 scale_fill_manual(values = getPalette(length(lists))) + 
 theme_minimal()
 print(plot)
+  
+}else{
+#PHYLOGENETIC DIVERSITY
+  
+if(class(tree) != "phylog"){tree.phylog <- phylo.to.phylog(tree)}
+#Run iNextPD
+sp.inextpd <- iNextPD(lists, q=qvalue, datatype="incidence_freq",phy=tree.phylog,size=seq(1,maxsize*3,round(maxsize*3/20)))
+  
+#Extract data from iNextPD object
+table <- c()
+for(subsystem in c(1:length(lists))){
+row <- cbind(rep(names(sp.inextpd$iNextPDEst[subsystem]),nrow((sp.inextpd$iNextPDEst[[subsystem]]))),sp.inextpd$iNextPDEst[[subsystem]][,1],sp.inextpd$iNextPDEst[[subsystem]][,2],sp.inextpd$iNextPDEst[[subsystem]][,4])
+table <- rbind(table,row)
+}
+melted.inextpd <- as.data.frame(table)
+melted.inextpd[,2] <- as.numeric(as.character(melted.inextpd[,2]))
+melted.inextpd[,4] <- as.numeric(as.character(melted.inextpd[,4]))
+colnames(melted.inextpd) <- c("Subsystem","Size","Method","Diversity")
+
+#Plot
+getPalette = colorRampPalette(brewer.pal(length(lists), "Paired"))
+plot <- ggplot() +
+geom_line(data = melted.inextpd[which(melted.inextpd$Method == "interpolated"),], aes(x = Size, y = Diversity, colour=Subsystem)) +
+geom_line(data = melted.inextpd[which(melted.inextpd$Method == "extrapolated"),], aes(x = Size, y = Diversity, colour=Subsystem), linetype=2) + 
+geom_point(data = melted.inextpd[which(melted.inextpd$Method == "observed"),],aes(x = Size, y = Diversity, colour=Subsystem)) +
+xlab("Sample size") + 
+ylab("Diversity") +
+scale_colour_manual(values = getPalette(length(lists))) + 
+scale_fill_manual(values = getPalette(length(lists))) + 
+theme_minimal()
+print(plot)
+  
+}
 
 }

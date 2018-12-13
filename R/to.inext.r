61ff889d3f4cb764c03cf9808940afe7ac902283
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-12-13 18:43:32 +0100

    Update to.inext.r

--- R/to.inext.r
+++ R/to.inext.r
@@ -1,9 +1,16 @@
-to.inext <- function(otutable,hierarchy){
+to.inext <- function(otutable,hierarchy,type){
 
+if(missing(type)){type="incidence_raw"}
+	
 #Without hierarchy (single list)
 if(missing(hierarchy)){
-list <- c(ncol(otutable.subset),rev(sort(rowSums(otutable.subset != 0))))
-return(results)
+if(type == "incidence_raw"){
+otutable[which(otutable != 0)] <- 1
+result <- otutable}
+if(type == "incidence_freq"){
+result <- c(ncol(otutable),rev(sort(rowSums(otutable != 0))))}
+result <- result[result > 0]
+return(result)
 }
 
 #With hierarchy (multiple lists)
@@ -16,7 +23,10 @@ for (g in groups){
 	samples <- as.character(hierarchy[which(hierarchy$Group == g),1])
 	otutable.subset <- otutable[,samples]
 	otutable.subset <- as.data.frame(otutable.subset[apply(otutable.subset, 1, function(z) !all(z==0)),])
-	list.subset <- c(ncol(otutable.subset),rev(sort(rowSums(otutable.subset != 0))))
+	if(type == "incidence_raw"){
+	otutable.subset[which(otutable.subset != 0)] <- 1}
+	if(type == "incidence_freq"){
+	list.subset <- c(ncol(otutable.subset),rev(sort(rowSums(otutable.subset != 0))))}
 	results[[g]] <- list.subset
 }
 }

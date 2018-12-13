fb95a661810375420e53e4994edbab680644bc79
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-12-13 17:28:47 +0100

    Update hill.intext.r

--- R/hill.intext.r
+++ R/hill.intext.r
@@ -23,21 +23,20 @@ sp.inext <- iNextPD(lists, q=qvalue, datatype="incidence_freq",phy=tree.phylog,s
 #Extract data from iNEXT object
 table <- c()
 for(subsystem in c(1:length(lists))){
-row <- cbind(rep(names(sp.inext$iNextEst[subsystem]),nrow((sp.inext$iNextEst[[subsystem]]))),sp.inext$iNextEst[[subsystem]][,1],sp.inext$iNextEst[[subsystem]][,2],sp.inext$iNextEst[[subsystem]][,4],table(hierarchy[,2])[subsystem],sp.inext$iNextEst[[subsystem]][which(sp.inext$iNextEst[[subsystem]][,2] == "observed"),4])
+row <- cbind(rep(names(sp.inext$iNextEst[subsystem]),nrow((sp.inext$iNextEst[[subsystem]]))),sp.inext$iNextEst[[subsystem]][,1],sp.inext$iNextEst[[subsystem]][,2],sp.inext$iNextEst[[subsystem]][,4])
 table <- rbind(table,row)
 }
 melted.inext <- as.data.frame(table)
 melted.inext[,2] <- as.numeric(as.character(melted.inext[,2]))
 melted.inext[,4] <- as.numeric(as.character(melted.inext[,4]))
-melted.inext[,5] <- as.numeric(as.character(melted.inext[,5]))
-melted.inext[,6] <- as.numeric(as.character(melted.inext[,6]))
-colnames(melted.inext) <- c("Subsystem","Size","Method","Diversity","Sample_size","Observed_diversity")
+colnames(melted.inext) <- c("Subsystem","Size","Method","Diversity")
 
 #Plot
 getPalette = colorRampPalette(brewer.pal(length(lists), "Paired"))
-plot <- ggplot(melted.inext , aes(x = Size, y = Diversity, group=Subsystem, colour=Subsystem)) +
-geom_line() + 
-geom_point(aes(x = Sample_size, y = Observed_diversity, fill=Subsystem)) +
+plot <- ggplot() +
+geom_line(data = melted.inext[which(melted.inext$Method == "interpolated"),], aes(x = Size, y = Diversity, colour=Subsystem)) +
+geom_line(data = melted.inext[which(melted.inext$Method == "extrapolated"),], aes(x = Size, y = Diversity, colour=Subsystem), linetype=2) + 
+geom_point(data = melted.inext[which(melted.inext$Method == "observed"),],aes(x = Size, y = Diversity, colour=Subsystem)) +
 xlab("Sample size") + 
 ylab("Diversity") +
 scale_colour_manual(values = getPalette(length(lists))) + 

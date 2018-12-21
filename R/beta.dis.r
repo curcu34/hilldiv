6e88d3ab64ea57e5f943635a0f2c8bcf4a36a4e4
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-12-21 12:50:53 +0100

    Update beta.dis.r

--- R/beta.dis.r
+++ R/beta.dis.r
@@ -6,12 +6,12 @@ if(class(beta) == "numeric"){input="beta"}
 if(class(beta) == "list"){
 input="list"
 if(names(beta[3]) != "Order_diversity") stop("The input object is not valid")
-  if((beta[[1]] == 2) && (beta[[2]] == "abundance")){
+  if(beta[[1]] == 2){
     qvalue <- beta[[3]]
     N <- beta[[4]]
     beta <- beta[[7]]
   }
-  if((beta[[1]] == 3) || (beta[[2]] == "incidence")){
+  if(beta[[1]] == 3){
     qvalue <- beta[[3]]
     N1 <- beta[[4]]
     N2 <- beta[[5]]

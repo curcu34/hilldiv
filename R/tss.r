58203abe34d049a834700e755c81674c7ad325fa
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-12-21 15:54:38 +0100

    Update tss.r

--- R/tss.r
+++ R/tss.r
@@ -1,9 +1,1 @@
-tss <- function(abund){
-  #If input data is a vector
-  if(is.null(dim(abund)) == "TRUE"){ 
-  abund.norm <- abund/sum(abund)}
-  #If input data is an OTU table
-  if(is.null(dim(abund)) == "FALSE"){
-  abund.norm <- sweep(abund, 2, colSums(abund), FUN="/")}
-  return(abund.norm)
-}
+tss <- function(q){}

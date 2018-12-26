5958994f1832c8abb6d59a7da1e1bae15687f6b0
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-12-26 10:57:28 +0100

    Update and rename tss.r to norm.r

---
+++ R/norm.r
@@ -0,0 +1,9 @@
+norm <- function(abund){
+  #If input data is a vector
+  if(is.null(dim(abund)) == "TRUE"){ 
+  abund.norm <- abund/sum(abund)}
+  #If input data is an OTU table
+  if(is.null(dim(abund)) == "FALSE"){
+  abund.norm <- sweep(abund, 2, colSums(abund), FUN="/")}
+  return(abund.norm)
+}

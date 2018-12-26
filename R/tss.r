fe0678ef0c0d561fae7ca467a4325521bf10875f
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-12-26 11:11:52 +0100

    Delete tss.r

--- R/tss.r
+++
@@ -1,7 +0,0 @@
-tss <- function(abund){
-  if(is.null(dim(abund)) == "TRUE"){ 
-  abund.norm <- abund/sum(abund)}
-  if(is.null(dim(abund)) == "FALSE"){
-  abund.norm <- sweep(abund, 2, colSums(abund), FUN="/")}
-  return(abund.norm)
-}

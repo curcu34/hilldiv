432915bb23c8299e0ad0d49f283d5d573883d8e3
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-12-27 15:36:32 +0100

    Update div.part.r

--- R/div.part.r
+++ R/div.part.r
@@ -141,28 +141,28 @@ otutable.inext.L3 <- to.inext(otutable,type="incidence_raw")
 
 if(missing(tree)){  
   #Alpha diversity
-  if(qvalue == 0){L2_est.div <- mean(ChaoSpecies(otutable.inext.L2,datatype="incidence_raw", conf=0.95)[,2])}
-  if(qvalue == 1){L2_est.div <- exp(mean(ChaoEntropy(otutable.inext.L2,datatype="incidence_raw", transform=FALSE, conf=0.95)[,2]))}
-  if(qvalue == 2){L2_est.div <- 1/(1-mean(EstSimpson(otutable.inext.L2,datatype="incidence_raw", transform=FALSE, conf=0.95)[,2]))}
+  if(qvalue == 0){L2_div <- mean(ChaoSpecies(otutable.inext.L2,datatype="incidence_raw", conf=0.95)[,2])}
+  if(qvalue == 1){L2_div <- exp(mean(ChaoEntropy(otutable.inext.L2,datatype="incidence_raw", transform=FALSE, conf=0.95)[,2]))}
+  if(qvalue == 2){L2_div <- 1/(1-mean(EstSimpson(otutable.inext.L2,datatype="incidence_raw", transform=FALSE, conf=0.95)[,2]))}
   #Gamma diversity  
-  if(qvalue == 0){L3_est.div <- ChaoSpecies(otutable.inext.L3,datatype="incidence_raw", conf=0.95)[,2]}
-  if(qvalue == 1){L3_est.div <- ChaoEntropy(otutable.inext.L3,datatype="incidence_raw", transform=TRUE, conf=0.95)[,2]}
-  if(qvalue == 2){L3_est.div <- EstSimpson(otutable.inext.L3,datatype="incidence_raw", transform=TRUE, conf=0.95)[,2]}
+  if(qvalue == 0){L3_div <- ChaoSpecies(otutable.inext.L3,datatype="incidence_raw", conf=0.95)[,2]}
+  if(qvalue == 1){L3_div <- ChaoEntropy(otutable.inext.L3,datatype="incidence_raw", transform=TRUE, conf=0.95)[,2]}
+  if(qvalue == 2){L3_div <- EstSimpson(otutable.inext.L3,datatype="incidence_raw", transform=TRUE, conf=0.95)[,2]}
 
 }else{
   tree.phylog <- phylo.to.phylog(tree)
   #Alpha diversity
-  if(qvalue == 0){L2_est.div <- mean(estPD(otutable.inext.L2,labels=rownames(otutable),phy=tree.phylog,q=0,datatype="incidence_raw", se=FALSE, conf=0.95)[,2])}
-  if(qvalue == 1){L2_est.div <- exp(mean(log(estPD(otutable.inext.L2,labels=rownames(otutable),phy=tree.phylog,q=1,datatype="incidence_raw", se=FALSE, conf=0.95)[,2])))}
+  if(qvalue == 0){L2_div <- mean(estPD(otutable.inext.L2,labels=rownames(otutable),phy=tree.phylog,q=0,datatype="incidence_raw", se=FALSE, conf=0.95)[,2])}
+  if(qvalue == 1){L2_div <- exp(mean(log(estPD(otutable.inext.L2,labels=rownames(otutable),phy=tree.phylog,q=1,datatype="incidence_raw", se=FALSE, conf=0.95)[,2])))}
   if(qvalue == 2){
-    L2_est.div.raw <- estPD(otutable.inext.L2,labels=rownames(otutable),phy=tree.phylog,q=2,datatype="incidence_raw", se=FALSE, conf=0.95)[,2]}
-    L2_est.div.raw2 <- mean(-(1-L2_est.div.raw)/L2_est.div.raw)
-    L2_est.div <- 1/(1-L2_est.div.raw2)}
+    L2_div.raw <- estPD(otutable.inext.L2,labels=rownames(otutable),phy=tree.phylog,q=2,datatype="incidence_raw", se=FALSE, conf=0.95)[,2]}
+    L2_div.raw2 <- mean(-(1-L2_div.raw)/L2_div.raw)
+    L2_div <- 1/(1-L2_div.raw2)}
   #Gamma diversity  
-    L3_est.div <- estPD(otutable.inext.L3,labels=rownames(otutable),phy=tree.phylog,q=qvalue,datatype="incidence_raw", se=FALSE, conf=0.95)
+    L3_div <- estPD(otutable.inext.L3,labels=rownames(otutable),phy=tree.phylog,q=qvalue,datatype="incidence_raw", se=FALSE, conf=0.95)
   }
   #L2-L3, beta and sample size
-  beta <- L3_est.div/L2_est.div
+  beta <- L3_div/L2_div
   N <- length(otutable.inext.L2)
   
   #Return values

5c930024f277403f79cd3c69ca9870b6809a9553
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2018-12-27 16:19:48 +0100

    Update hill.div.r

--- R/hill.div.r
+++ R/hill.div.r
@@ -1,15 +1,17 @@
-hill.div <- function(abund,qvalue,tree){ 
+hill.div <- function(abund,qvalue,tree,type){ 
   
 #Quality-check and warnings
 if(missing(abund)) stop("Abundance data is missing")
 if(missing(qvalue)) stop("q value is missing")
 if(qvalue==1){qvalue=0.99999} 
 if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
-
+if(missing(type)){type == "abundance"}
+  
 #Diversity
 if(missing(tree)){
     #If input data is a vector
     if(is.null(dim(abund)) == TRUE){
+    if(type == "incidence") stop("Incidence-based analysis requires an OTU table with multiple samples")
     if(sum(abund) != "1") {abund <- tss(abund)}
     pi <- abund[abund!=0] 
     div <- sum(pi^qvalue)^(1/(1-qvalue))
@@ -18,18 +20,28 @@ if(missing(tree)){
 
     #If input data is an OTU table
     if(is.null(dim(abund)) == FALSE){
-    if(sum(colSums(abund)) != ncol(abund)) {abund <- tss(abund)}
-    if(is.null(colnames(abund)) == TRUE){colnames(abund) <- seq(1:ncol(abund))}
-    samples <- colnames(abund)
-    sample.vector <- c()
-    for (s in samples){
-      vector <- abund[,s]
-      pi <- vector[vector!=0] 
-      div <- sum(pi^qvalue)^(1/(1-qvalue))
-      names(div) <- s
-      sample.vector <- c(sample.vector,div)
+    if(type == "abundance"){
+      if(sum(colSums(abund)) != ncol(abund)) {abund <- tss(abund)}
+     if(is.null(colnames(abund)) == TRUE){colnames(abund) <- seq(1:ncol(abund))}
+     samples <- colnames(abund)
+     sample.vector <- c()
+      for (s in samples){
+        vector <- abund[,s]
+        pi <- vector[vector!=0] 
+        div <- sum(pi^qvalue)^(1/(1-qvalue))
+       names(div) <- s
+       sample.vector <- c(sample.vector,div)
+      }
+      return(sample.vector) 
+    }else if(type == "incidence"){
+    pi <- tss(rowSums(abund != 0))
+    pi <- pi[pi!=0] 
+    div <- sum(pi^qvalue)^(1/(1-qvalue))
+    return(div) 
+    }else{
+    stop("The provided type of analysis is incorrect")
     }
-    return(sample.vector) 
+   
     }
 #Phylodiversity
 }else{

88d385d6681e1fcb916a51c9111dec55227b28eb
anttonalberdi <37664231+anttonalberdi@users.noreply.github.com>
2019-01-21 06:48:46 +0100

    Update div.test.plot.r

--- R/div.test.plot.r
+++ R/div.test.plot.r
@@ -1,38 +1,32 @@
-div.test.plot <- function(otutable,qvalue,hierarchy,tree,chart){ 
-if(missing(otutable)) stop("OTU table is missing")
-if(is.null(dim(otutable)) == TRUE) stop("The OTU table is not a matrix")
-if(dim(otutable)[1] < 2) stop("The OTU table only less than 2 OTUs")
-if(dim(otutable)[2] < 2) stop("The OTU table contains less than 2 samples")
-if(missing(qvalue)) stop("q value is missing")
-if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
-if(missing(hierarchy)) stop("Hierarchy table is necessary to contrast groups of samples")
-if(missing(chart)) {chart="boxplot"}
+div.test.plot <- function(divtest,chart,colour){ 
 
-if(missing(tree)){
-div.values <- hill.div(otutable,qvalue)
-}else{
-if(class(tree) != "phylo") stop("Tree needs to be an object of class Phylo")  
-if(identical(sort(rownames(otutable)),sort(tree$tip.label)) == FALSE) stop("OTU names in the OTU table and tree do not match")
-div.values <- hill.div(otutable,qvalue,tree)
+if((names(divtest)[1] != "data") % (names(divtest)[2] != "normality.pvalue")) stop("The input object does not seem to be a div.test output.")
+
+#Get data table  
+divtestdata <- divtest$data  
+  
+#Declare colours
+if(missing(colour) || (length(colour) != divtest$groups)){
+getPalette <- colorRampPalette(brewer.pal(divtest$groups, "Paired"))
+colour <- getPalette(divtest$groups)
 }
-div.values.groups <- merge(t(t(div.values)),hierarchy,by.x="row.names",by.y="Sample")
-colnames(div.values.groups) <- c("Sample","Value","Group")
-
+  
 #Plot
 if(chart == "boxplot"){
-getPalette = colorRampPalette(brewer.pal(length(unique(div.values.groups$Group)), "Paired"))
-plot <- ggplot(div.values.groups, aes(x=Group, y=Value, colour=Group)) + 
+plot <- ggplot(divtestdata, aes(x=Group, y=Value, colour=Group, fill=Group)) + 
   geom_boxplot() +
   xlab("Effective number of OTUs") + ylab("Groups") +
+  scale_colour_manual(values=colour) +
+  scale_fill_manual(values=alpha(colour, 0.3)) +
   theme_minimal() +
   coord_flip()
 print(plot)
 }
 if(chart == "jitter"){
-getPalette = colorRampPalette(brewer.pal(length(unique(div.values.groups$Group)), "Paired"))
-plot <- ggplot(div.values.groups, aes(x=Group, y=Value, colour=Group)) + 
+plot <- ggplot(divtestdata, aes(x=Group, y=Value, colour=Group)) + 
   geom_jitter(width = 0.1) +
   xlab("Effective number of OTUs") + ylab("Groups") +
+  scale_colour_manual(values=alpha(colour, 0.6)) +
   theme_minimal() +
   coord_flip()
 print(plot)

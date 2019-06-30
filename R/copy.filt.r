123110930ed12bee1dd4158935e6ee37f90619c5
“anttonalberdi” <anttonalberdi@gmail.com>
2019-06-30 10:55:35 +0200

    Copy.filt manual

--- R/copy.filt.r
+++ R/copy.filt.r
@@ -2,11 +2,23 @@ copy.filt <- function(abund,threshold){
   if(is.null(dim(abund)) == FALSE){
   #It is an OTU table (multiple samples)
   sapply(1:ncol(abund), function(colnum){temp = abund[,colnum]
-  rownums = which(temp < sum(temp)*threshold)
+    if(threshold == round(threshold)){
+    #Absolute
+    rownums = which(temp < threshold)
+    }else{
+    #Relative
+    rownums = which(temp < sum(temp)*threshold)
+    }
   abund[rownums, colnum] <<- 0})
   }else{
   #It is a vector (single sample)
-  abund[abund < sum(abund)*threshold] <- 0
+    if(threshold == round(threshold)){
+    #Absolute
+    abund[abund < threshold] <- 0
+    }else{
+    #Relative
+    abund[abund < sum(abund)*threshold] <- 0
+    }
   }
   return(abund)
 }

11d5a55320eb1bd5298b4a0485dc68f206d22905
“anttonalberdi” <anttonalberdi@gmail.com>
2019-08-10 09:32:54 +0200

    Change warning to message

--- R/match.data.r
+++ R/match.data.r
@@ -9,26 +9,26 @@ match.data <- function(otutable,tree,output){
   tree.otus <- tree$tip.label
 
 if(missing(output)){
-  if((length(setdiff(tree.otus,otutable.otus)) == 0) & (length(setdiff(otutable.otus,tree.otus)) == 0)){stop("OTUs in the OTU table and OTU tree match perfectly.")}
-  if((length(setdiff(tree.otus,otutable.otus)) > 0) & (length(setdiff(otutable.otus,tree.otus)) == 0)){stop("The OTU tree contains OTUs absent in the OTU table. Filter the OTU tree")}
-  if((length(setdiff(tree.otus,otutable.otus)) == 0) & (length(setdiff(otutable.otus,tree.otus)) > 0)){stop("The OTU tree contains OTUs absent in the OTU table. Filter the OTU tree")}
-  if((length(setdiff(tree.otus,otutable.otus)) > 0) & (length(setdiff(otutable.otus,tree.otus)) > 0)){stop("The OTU table contains OTUs absent in the OTU tree and the OTU tree contains OTUs absent in the OTU table. Filter both files")}
+  if((length(setdiff(tree.otus,otutable.otus)) == 0) & (length(setdiff(otutable.otus,tree.otus)) == 0)){message("OTUs in the OTU table and OTU tree match perfectly.")}
+  if((length(setdiff(tree.otus,otutable.otus)) > 0) & (length(setdiff(otutable.otus,tree.otus)) == 0)){message("The OTU tree contains OTUs absent in the OTU table. Filter the OTU tree")}
+  if((length(setdiff(tree.otus,otutable.otus)) == 0) & (length(setdiff(otutable.otus,tree.otus)) > 0)){message("The OTU tree contains OTUs absent in the OTU table. Filter the OTU tree")}
+  if((length(setdiff(tree.otus,otutable.otus)) > 0) & (length(setdiff(otutable.otus,tree.otus)) > 0)){message("The OTU table contains OTUs absent in the OTU tree and the OTU tree contains OTUs absent in the OTU table. Filter both files")}
 }
 
 #Output OTU table
 if(output == "otutable"){
   if(length(setdiff(tree.otus,otutable.otus)) > 0){
-    warning("All OTUs present in the OTU table are present in the tree, but the tree contains OTUs absent in the OTU table. Remember to filter the tree.")
+    message("All OTUs present in the OTU table are present in the tree, but the tree contains OTUs absent in the OTU table. Remember to filter the tree.")
   }
   if(length(setdiff(otutable.otus,tree.otus)) > 0){
     OTUs.to.drop <- setdiff(otutable.otus,tree.otus)
     otutable.filt <- otutable[!(row.names(otutable) %in% OTUs.to.drop), ]
     OTUs.to.drop.string <- paste(OTUs.to.drop,collapse=", ")
-    warning("The following OTUs were removed from the OTU table for being absent in the tree: ",OTUs.to.drop.string)
+    message("The following OTUs were removed from the OTU table for being absent in the tree: ",OTUs.to.drop.string)
     return(otutable.filt)
   }
   if((length(setdiff(tree.otus,otutable.otus)) == 0) & (length(setdiff(otutable.otus,tree.otus)) == 0)){
-    stop("OTUs in the OTU table and OTU tree match perfectly. No new OTU table was created.")
+    message("OTUs in the OTU table and OTU tree match perfectly. No new OTU table was created.")
   }
 
 }
@@ -39,14 +39,14 @@ if(output == "tree"){
     OTUs.to.drop <- setdiff(tree.otus,otutable.otus)
     tree.filt <- drop.tip(tree,OTUs.to.drop)
     OTUs.to.drop.string <- paste(OTUs.to.drop,collapse=", ")
-    warning("The following OTUs were removed from the tree for being absent in the OTU table: ",OTUs.to.drop.string)
+    message("The following OTUs were removed from the tree for being absent in the OTU table: ",OTUs.to.drop.string)
     return(tree.filt)
   }
   if(length(setdiff(otutable.otus,tree.otus)) > 0){
-    warning("All OTUs present in the tree are present in the OTU table, but the OTU table contains OTUs absent in the tree. Remember to filter the OTU table.")
+    message("All OTUs present in the tree are present in the OTU table, but the OTU table contains OTUs absent in the tree. Remember to filter the OTU table.")
   }
   if((length(setdiff(tree.otus,otutable.otus)) == 0) & (length(setdiff(otutable.otus,tree.otus)) == 0)){
-    stop("OTUs in the OTU tree and OTU table match perfectly. No new tree was created.")
+    message("OTUs in the OTU tree and OTU table match perfectly. No new tree was created.")
   }
 }
 

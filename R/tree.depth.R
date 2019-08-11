bb22be72984813b0a2062684712e779c78bd01a2
Antton Alberdi <37664231+anttonalberdi@users.noreply.github.com>
2019-08-11 18:29:22 +0200

    Update tree.depth.R

--- R/tree.depth.R
+++ R/tree.depth.R
@@ -1,6 +1,13 @@
 tree.depth <- function(tree,abund){
-if(is.null(dim(abund)) == TRUE){vector <- tss(abund)}  
-if(is.null(dim(abund)) == FALSE){vector <- tss(abund[,1])}  
+if(class(tree) != "phylo") stop("Tree needs to be an object of class Phylo")  
+if(is.null(dim(abund)) == TRUE){
+  vector <- abund
+  }  
+if(is.null(dim(abund)) == FALSE){
+  vector <- abund[,1]
+  names(vector) <- rownames(abund)
+  } 
+if(identical(sort(names(vector)),sort(tree$tip.label)) == FALSE) stop("OTU names in the vector and tree do not match")  
 vector <- tss(vector)
 Li <- tree$edge.length
 ltips <- sapply(tree$edge[, 2], function(node) geiger::tips(tree, node))

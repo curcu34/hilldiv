c4d747c1191afb0a3fd43945bb60d968afaeb060
Antton Alberdi <37664231+anttonalberdi@users.noreply.github.com>
2019-08-11 18:26:00 +0200

    Update tree.depth.R

--- R/tree.depth.R
+++ R/tree.depth.R
@@ -1,6 +1,6 @@
 tree.depth <- function(tree,abund){
-if(is.null(dim(abund)) == TRUE){vector <- abund}  
-if(is.null(dim(abund)) == FALSE){vector <- abund[,1]}  
+if(is.null(dim(abund)) == TRUE){vector <- tss(abund)}  
+if(is.null(dim(abund)) == FALSE){vector <- tss(abund[,1])}  
 vector <- tss(vector)
 Li <- tree$edge.length
 ltips <- sapply(tree$edge[, 2], function(node) geiger::tips(tree, node))

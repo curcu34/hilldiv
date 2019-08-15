bd2f9e6930861534222814f76cc42c0280158fd5
“anttonalberdi” <anttonalberdi@gmail.com>
2019-08-15 17:32:49 +0200

    s

--- R/tree.depth.R
+++ R/tree.depth.R
@@ -13,15 +13,15 @@
 #' @export
 
 tree.depth <- function(tree,abund){
-if(class(tree) != "phylo") stop("Tree needs to be an object of class Phylo")  
+if(class(tree) != "phylo") stop("Tree needs to be an object of class Phylo")
 if(is.null(dim(abund)) == TRUE){
   vector <- abund
-  }  
+  }
 if(is.null(dim(abund)) == FALSE){
   vector <- abund[,1]
   names(vector) <- rownames(abund)
-  } 
-if(identical(sort(names(vector)),sort(tree$tip.label)) == FALSE) stop("OTU names in the vector and tree do not match")  
+  }
+if(identical(sort(names(vector)),sort(tree$tip.label)) == FALSE) stop("OTU names in the vector and tree do not match")
 vector <- tss(vector)
 Li <- tree$edge.length
 ltips <- sapply(tree$edge[, 2], function(node) geiger::tips(tree, node))

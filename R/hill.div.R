af6b5ddb471fef4bd55d3dcc1db49cf4d22f7ab5
“anttonalberdi” <anttonalberdi@gmail.com>
2019-08-16 09:54:38 +0200

    hill.div() update

--- R/hill.div.R
+++ R/hill.div.R
@@ -3,10 +3,10 @@
 #' @author Antton Alberdi, \email{anttonalberdi@gmail.com}
 #' @keywords OTU ASV diversity
 #' @description Compute neutral or phylogenetic Hill numbers from a single sample (vector) or count table (matrix). Hill numbers or numbers equivalents of diversity indices are diversity measures that compute diversity in effective number of OTUs, i.e. the number of equally abundant OTUs that would be needed to give the same value of diversity.
-#' @param abund A vector or a matrix/data.frame indicating the relative abundances of one or multiple samples, respectively. If a matrix/data.frame is provided, columns must refer to samples and rows to OTUs.
+#' @param count A vector or a matrix/data.frame indicating the (relative) counts of one or multiple samples, respectively. If a matrix/data.frame is provided, columns must refer to samples and rows to OTUs.
 #' @param qvalue A positive integer or decimal number (>=0), usually between 0 and 3.
 #' @param tree An ultrametic tree of class 'phylo'. The tip labels must match the names of the vector values (if one sample) or matrix rows (if multiple samples).
-#' @param type Either "abundance" for abundance-based Hill number computation or "incidence" for incidence-based Hill number computation. In the latter case, the 'abund' object needs to be an OTU table, as incidence-based Hill number computation requires multiple samples. Default: type="abundance".
+#' @param dist A dist object indicating the pairwise distances between samples. NOT implemented yet
 #' @seealso \code{\link{index.div}}, \code{\link{div.part}}
 #' @examples
 #' hill.div(otu.vector,0)
@@ -14,115 +14,98 @@
 #' hill.div(otu.table,1,tree)
 #' hill.div(otu.table,1,tree,type="incidence")
 #' hill.div(otu.table,qvalue=2,tree=tree)
+#' hill.div(otu.table,qvalue=2,traits=dist) #To be implemented soon
 #' @references
 #' Alberdi, A., Gilbert, M.T.P. (2019). A guide to the application of Hill numbers to DNA-based diversity analyses. Molecular Ecology Resources. Early view.\cr\cr
 #' Jost, L. (2006). Entropy and diversity. Oikos, 113, 363–375.\cr\cr
 #' Hill, M. O. (1973). Diversity and evenness: a unifying notation and its con‐ sequences. Ecology, 54, 427–432.
 #' @export
 
-hill.div <- function(abund,qvalue,tree,type){ 
-  
+hill.div <- function(count,qvalue,tree,dist){
+
 #Quality-check and warnings
-if(missing(abund)) stop("Abundance data is missing")
+if(missing(count)) stop("Count data is missing")
 if(missing(qvalue)) stop("q value is missing")
-if(qvalue==1){qvalue=0.99999} 
+if(qvalue==1){qvalue=0.99999}
 if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
-if(missing(type)){type="abundance"}
-if((type=="incidence") & (length(dim(abund)) != 2)) stop("Incidence-based Hill number computation requires multiple samples")
-  
-#Diversity
-if(missing(tree)){
-    #If input data is a vector
-    if(is.null(dim(abund)) == TRUE){
-    if(type == "incidence") stop("Incidence-based analysis requires an OTU table with multiple samples")
-    if(sum(abund) != "1") {abund <- tss(abund)}
-    pi <- abund[abund!=0] 
-    div <- sum(pi^qvalue)^(1/(1-qvalue))
-    return(div) 
-    }
 
-    #If input data is an OTU table
-    if(is.null(dim(abund)) == FALSE){
-    if(type == "abundance"){
-     if(sum(colSums(abund)) != ncol(abund)) {abund <- tss(abund)}
-     if(is.null(colnames(abund)) == TRUE){colnames(abund) <- seq(1:ncol(abund))}
-     samples <- colnames(abund)
-     sample.vector <- c()
-      for (s in samples){
-        vector <- abund[,s]
-        pi <- vector[vector!=0] 
-        div <- sum(pi^qvalue)^(1/(1-qvalue))
-       names(div) <- s
-       sample.vector <- c(sample.vector,div)
-      }
-      return(sample.vector) 
-    }else if(type == "incidence"){
-    #Incidence-based  
-    pi <- tss(rowSums(abund != 0))
-    pi <- pi[pi!=0] 
-    div <- sum(pi^qvalue)^(1/(1-qvalue))
-    return(div) 
-    }else{
-    stop("The provided type of analysis is incorrect")
-    }
-   
-    }
-#Phylodiversity
+#Hill numbers type definition
+if(missing(tree) && missing(dist)){hilltype="neutral"}
+if(!missing(tree) && missing(dist)){hilltype="phylogenetic"}
+if(missing(tree) && !missing(dist)){hilltype="functional"}
+if(!missing(tree) && !missing(dist)) stop("Phylogenetic and trait information cannot be added at once. Use either phylogenetic (tree) or functional (traits) information.")
+
+#Input data type definition
+if(is.null(dim(count)) == TRUE){
+inputtype="onesample"
 }else{
-    if(class(tree) != "phylo") stop("Tree needs to be an object of class Phylo")  
-    if(ape::is.ultrametric(tree) == FALSE) stop("Tree needs to be ultrametric")  
-
-    #If input data is a vector
-    if(is.null(dim(abund)) == TRUE){
-    if(type == "incidence") stop("Incidence-based analysis requires an OTU table with multiple samples")
-    if(identical(sort(names(abund)),sort(tree$tip.label)) == FALSE) stop("OTU names in the vector and tree do not match")  
-    if(sum(abund) != "1") {abund <- tss(abund)}
-    Li <- tree$edge.length #Get branch lengths
-    ltips <- sapply(tree$edge[, 2], function(node) geiger::tips(tree, node)) #Sum relative abundances per lineage
-    ai <- unlist(lapply(ltips, function(TipVector) sum(abund[TipVector]))) #Sum relative abundances per lineage
-    T <- sum(Li * ai) #Get total tree depth
-    Li <- Li[ai != 0] #Remove zeros
-    ai <- ai[ai != 0] #Remove zeros
-    phylodiv <- sum(Li/T * ai^qvalue)^(1/(1-qvalue)) #Compute phylodiversity
-    return(phylodiv)
-    }
-
-    #If input data is an OTU table
-    if(is.null(dim(abund)) == FALSE){    
-    if(identical(sort(rownames(abund)),sort(tree$tip.label)) == FALSE) stop("OTU names in the OTU table and tree do not match")  
-    if(type == "abundance"){
-      #Abundance-based  
-      if(sum(colSums(abund)) != ncol(abund)) {abund <- tss(abund)}
-      samples <- colnames(abund)
-      sample.vector <- c()
-      for (s in samples){
-          vector <- abund[,s]
-          names(vector) <- rownames(abund)
-         Li <- tree$edge.length #Get branch lengths
-         ltips <- sapply(tree$edge[, 2], function(node) geiger::tips(tree, node)) #Sum relative abundances per lineage
-          ai <- unlist(lapply(ltips, function(TipVector) sum(vector[TipVector]))) #Sum relative abundances per lineage
-          T <- sum(Li * ai) #Get total tree depth
-          Li <- Li[ai != 0] #Remove zeros
-          ai <- ai[ai != 0] #Remove zeros
-          phylodiv <- sum(Li/T * ai^qvalue)^(1/(1-qvalue)) #Compute phylodiversity
-          names(phylodiv) <- s
-          sample.vector <- c(sample.vector,phylodiv)
-          }      
-     return(sample.vector) 
-    }else if(type == "incidence"){
-    #Incidence-based  
-    pi <- tss(rowSums(abund != 0))
-    Li <- tree$edge.length #Get branch lengths
-    ltips <- sapply(tree$edge[, 2], function(node) geiger::tips(tree, node)) #Sum relative abundances per lineage
-    ai <- unlist(lapply(ltips, function(TipVector) sum(pi[TipVector]))) #Sum relative abundances per lineage
-    T <- sum(Li * ai) #Get total tree depth
-    Li <- Li[ai != 0] #Remove zeros
-    ai <- ai[ai != 0] #Remove zeros
-    phylodiv <- sum(Li/T * ai^qvalue)^(1/(1-qvalue)) #Compute phylodiversity
-    return(phylodiv)
-    }else{
-    stop("The provided type of analysis is incorrect")
-    }
-    }
+inputtype="multiplesamples"
 }
+
+###
+# Neutral Hill numbers
+###
+if(hilltype == "neutral"){
+
+#Function
+neutral.Hill <- function(vector,qvalue){
+    pi <- vector[vector!=0]
+    sum(pi^qvalue)^(1/(1-qvalue))
+    }
+
+#One sample
+if(inputtype == "onesample"){
+div <- neutral.Hill(tss(count),qvalue)
+return(div)
+}
+
+#Multiple samples
+if(inputtype == "multiplesamples"){
+divs <- apply(tss(count), 2, function(x) neutral.Hill(x,qvalue))
+return(divs)
+}
+
+}
+
+###
+# Phylogenetic Hill numbers
+###
+if(hilltype == "phylogenetic"){
+
+if(class(tree) != "phylo") stop("Tree needs to be an object of class Phylo")
+if(ape::is.ultrametric(tree) == FALSE) stop("Tree needs to be ultrametric")
+
+#Function
+phylogenetic.Hill <- function(vector,qvalue,tree){
+  Li <- tree$edge.length
+  ltips <- sapply(tree$edge[, 2], function(node) geiger::tips(tree, node))
+  ai <- unlist(lapply(ltips, function(TipVector) sum(vector[TipVector])))
+  T <- sum(Li * ai)
+  Li <- Li[ai != 0]
+  ai <- ai[ai != 0]
+  sum(Li/T * ai^qvalue)^(1/(1-qvalue))
+  }
+
+  #One sample
+  if(inputtype == "onesample"){
+  if(identical(sort(names(count)),sort(tree$tip.label)) == FALSE) stop("OTU names in the vector and tree do not match. Use match.data() to solve this issue.")
+  div <- phylogenetic.Hill(tss(count),qvalue,tree)
+  return(div)
+  }
+
+  #Multiple samples
+  if(identical(sort(rownames(count)),sort(tree$tip.label)) == FALSE) stop("OTU names in the vector and tree do not match. Use match.data() to solve this issue.")
+  if(inputtype == "multiplesamples"){
+  divs <- apply(tss(count), 2, function(x) phylogenetic.Hill(x,qvalue,tree))
+  return(divs)
+  }
+
+}
+
+###
+# Functional Hill numbers
+###
+if(hilltype == "functional") stop("Functional Hill numbers have not been implemented yet in the function.")
+
+#End of function
 }
--- documentation/hill.div.md
+++ documentation/hill.div.md
@@ -1,6 +1,6 @@
 # hill.div()
 
-Compute neutral or phylogenetic Hill numbers from a single sample (vector) or count table (matrix). Hill numbers or numbers equivalents of diversity indices are diversity measures that compute diversity in effective number of OTUs, i.e. the number of equally abundant OTUs that would be needed to give the same value of diversity. 
+Compute neutral or phylogenetic Hill numbers from a single sample (vector) or count table (matrix). Hill numbers or numbers equivalents of diversity indices are diversity measures that compute diversity in effective number of OTUs, i.e. the number of equally abundant OTUs that would be needed to give the same value of diversity.
 
 ## Arguments
 | Arguments | Description |
@@ -8,7 +8,7 @@ Compute neutral or phylogenetic Hill numbers from a single sample (vector) or co
 | abund | A vector or a matrix/data.frame indicating the relative abundances of one or multiple samples, respectively. If a matrix/data.frame is provided, columns must refer to samples and rows to OTUs. |
 | qvalue | A positive integer or decimal number (>=0), usually between 0 and 3. |
 | tree | An ultrametic tree of class 'phylo'. The tip labels must match the names of the vector values (if one sample) or matrix rows (if multiple samples). |
-| type | Either "abundance" for abundance-based Hill number computation or "incidence" for incidence-based Hill number computation. In the latter case, the 'abund' object needs to be an OTU table, as incidence-based Hill number computation requires multiple samples. Default: type="abundance".  |
+| dist | A dist object indicating the pairwise distances between samples. THIS FUNCTIONALITY IS NOT IMPLEMENTED YET.  |
 
 ## Examples
 

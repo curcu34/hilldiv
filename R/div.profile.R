3cd92216942751fe4ba03de0716cef8107281a78
“anttonalberdi” <anttonalberdi@gmail.com>
2019-08-22 18:34:41 +0200

    div.profile() and div.profile.plot() update

--- R/div.profile.R
+++ R/div.profile.R
@@ -6,149 +6,185 @@
 #' @param abund A vector or a matrix/data.frame indicating the relative abundances of one or multiple samples, respectively. If a matrix/data.frame is provided, columns must refer to samples and rows to OTUs.
 #' @param qvalues A vector of sequential orders of diversity (default from 0 to 5). order=seq(from = 0, to = 5, by = (0.1))
 #' @param tree A tree of class 'phylo'. The tip labels must match the names of the vector values (if one sample) or matrix rows (if multiple samples).
-#' @param values If 'TRUE' the Hill numbers for each order of diversity for each sample or group (if hierarchy table is provided) are printed on screen. values="FALSE".
 #' @param hierarchy A two-column matrix indicating the relation between samples (first column) and groups (second column).
-#' @param level Only if hierachy is used. If 'alpha', alpha diversity of the groups is plotted instead of gamma diversity. level="gamma"
-#' @param colour The number of vector items (colours, e.g. '#34k235'), must equal the number of samples or groups that are intended to plot.
-#' @param log If 'TRUE' the Hill numbers are transformed to the logarithmic scale. This is useful when there are large differences between q values (e.g. sharp drop from q=0 to q=1), which might complicate visualization. log="FALSE"
-#' @return A figure or a table of sequential diversity values.
-#' @seealso \code{\link{hill.div}}, \code{\link{div.part}}
+#' @param level Whether to compute alpha or gamma diversities of the system or the groups specified in the hierarchy table.
+#' @return A vector or matrix containing diversity values at different orders of diversity (as specified in qvalues).
+#' @seealso \code{\link{div.profile.plot}}, \code{\link{hill.div}}
 #' @examples
-#' div.profile(abund=otu.table[,1],qvalues=seq(from = 0, to = 5, by = (0.1)),tree=tree,values="TRUE")
+#' div.profile(abund=otu.table[,1],qvalues=seq(from = 0, to = 5, by = (0.1)),tree=tree)
 #' div.profile(vector)
-#' div.profile(otu.table,hierarchy=hierarchy.table)
+#' div.profile(otu.table,hierarchy=hierarchy.table,level="gamma")
 #' @references
 #' Alberdi, A., Gilbert, M.T.P. (2019). A guide to the application of Hill numbers to DNA-based diversity analyses. Molecular Ecology Resources. Early view.\cr\cr
 #' Chao, A., Chiu, C.‐H., & Jost, L. (2014). Unifying species diversity, phylo‐ genetic diversity, functional diversity, and related similarity and dif‐ ferentiation measures through hill numbers. Annual Review of Ecology Evolution and Systematics, 45, 297–324.
 #' @export
 
-div.profile <- function(abund,qvalues,tree,values,hierarchy,level,colour,log){
+div.profile <- function(abund,qvalues,tree,hierarchy,level){
 
 #Quality-check and warnings
 if(missing(abund)) stop("The abundance data is missing")
 if(missing(qvalues)) {qvalues= seq(from = 0, to = 5, by = (0.1))}
-if(missing(values)) {values= "FALSE"}
-if(missing(level)) {level= "gamma"}
-if(missing(log)) {log= "FALSE"}
+if(missing(level)) {level= "NA"}
 
-#If input data is a vector
+#If input data is a single sample (vector)
 if(is.null(dim(abund)) == TRUE){
     profile <- c()
     for (o in qvalues){
-    if(missing(tree)){ 
+    if(missing(tree)){
         div.value <- hilldiv::hill.div(abund,o)
         }else{
         div.value <- hilldiv::hill.div(abund,o,tree)
     }
     profile <- c(profile,div.value)
     }
-    profile.melted <- as.data.frame(cbind(qvalues,profile))
-    colnames(profile.melted) <- c("Order","Profile")
-    plot <- ggplot(profile.melted , aes(x = Order, y = Profile)) +
-           geom_line() + 
-           xlab("Order of diversity") + ylab("Effective number of OTUs") +
-           theme_minimal()
-    print(plot)
-    if(values == "TRUE"){
-    return(profile)
-    }
+    names(profile) <- qvalues
 }
 
-#If input data is an OTU table
+#Declare the fast version of hill.div if tree is inputed
+if(!missing(tree)){
+
+hill.div.fast <- function(count,qvalue,tree,dist){
+    if(qvalue==1){qvalue=0.99999}
+    phylogenetic.Hill.fast <- function(vector,qvalue,tree){
+    Li <- tree$edge.length
+    ai <- unlist(lapply(ltips, function(TipVector) sum(vector[TipVector])))
+    T <- sum(Li * ai)
+    Li <- Li[ai != 0]
+    ai <- ai[ai != 0]
+    sum(Li/T * ai^qvalue)^(1/(1-qvalue))
+    }
+    divs <- apply(tss(count), 2, function(x) phylogenetic.Hill.fast(x,qvalue,tree))
+    return(divs)
+}
+
+alpha.div.fast <- function(otutable,qvalue,tree,weight){
+if(missing(otutable)) stop("OTU table is missing")
+if(is.null(dim(otutable)) == TRUE) stop("The OTU table is not a matrix")
+if(dim(otutable)[1] < 2) stop("The OTU table only less than 2 OTUs")
+if(dim(otutable)[2] < 2) stop("The OTU table contains less than 2 samples")
+if(sum(colSums(otutable)) != ncol(otutable)) {otutable <- tss(otutable)}
+if(missing(qvalue)) stop("q value is missing")
+if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
+if(identical(sort(rownames(otutable)),sort(tree$tip.label)) == FALSE) stop("OTU names in the OTU table and tree do not match")
+if(ape::is.ultrametric(tree) == FALSE) stop("Tree needs to be ultrametric")
+if (qvalue==1) {qvalue=0.99999}
+if(missing(weight)) { weight= rep(1/ncol(otutable),ncol(otutable))}
+otutable <- as.data.frame(otutable)
+wj <- weight
+N <- ncol(otutable)
+Li <- tree$edge.length
+aij <- matrix(unlist(lapply(ltips, function(TipVector) colSums(otutable[TipVector,]))), ncol = N, byrow = TRUE)
+aij.wj <- sweep(aij, 2, wj, "*")
+T <- sum(sweep(aij.wj, 1, Li, "*"))
+L <- matrix(rep(Li, N), ncol = N)
+wm <-  matrix(rep(wj, length(Li)), ncol = N,byrow=TRUE)
+i <-  which(aij > 0)
+phylodiv <- sum(L[i] * (aij[i]*wm[i]/T)^qvalue)^(1/(1 - qvalue))/(N*T)
+return(phylodiv)
+}
+
+gamma.div.fast <- function(otutable,qvalue,tree,weight){
+if(missing(otutable)) stop("OTU table is missing")
+if(is.null(dim(otutable)) == TRUE) stop("The OTU table is not a matrix")
+if(dim(otutable)[1] < 2) stop("The OTU table only less than 2 OTUs")
+if(dim(otutable)[2] < 2) stop("The OTU table contains less than 2 samples")
+if(sum(colSums(otutable)) != ncol(otutable)) {otutable <- tss(otutable)}
+if(missing(qvalue)) stop("q value is missing")
+if(qvalue < 0) stop("q value needs to be possitive (equal or higher than zero)")
+if (qvalue==1) {qvalue=0.99999} # change q to the limit of the unity (0.99999) if q=1
+if(ape::is.ultrametric(tree) == FALSE) stop("Tree needs to be ultrametric")
+if(identical(sort(rownames(otutable)),sort(tree$tip.label)) == FALSE) stop("OTU names in the OTU table and tree do not match")
+if(missing(weight)) { weight= rep(1/ncol(otutable),ncol(otutable))}
+otutable <- as.data.frame(otutable)
+wj <- weight
+N <- ncol(otutable)
+Li <- tree$edge.length
+aij <- matrix(unlist(lapply(ltips, function(TipVector) colSums(otutable[TipVector,]))), ncol = N, byrow = TRUE)
+aij.wj <- sweep(aij, 2, wj, "*")
+ai <- rowSums(aij.wj)
+T <- sum(sweep(aij.wj, 1, Li, "*"))
+L <- matrix(rep(Li, N), ncol = N)
+Li <- Li[ai != 0] #Remove zeros
+ai <- ai[ai != 0] #Remove zeros
+wm <-  matrix(rep(wj, length(Li)), ncol = N, byrow=TRUE)
+phylodiv <- (sum(Li * (ai/T)^qvalue)^(1/(1 - qvalue)))/T
+return(phylodiv)
+}
+
+}
+
+#If input data is a count table (matrix)
 if(is.null(dim(abund)) == FALSE){
 
     if(dim(abund)[1] < 2) stop("The OTU table only less than 2 OTUs")
     if(dim(abund)[2] < 2) stop("The OTU table contains less than 2 samples")
 
-    profile <- c()
+    #Without hierarchy
     if(missing(hierarchy)){
+    profile <- c()
         for (o in qvalues){
             if(missing(tree)){
-            div.values <- hilldiv::hill.div(abund,o)
+            if(level == "NA"){div.values <- hilldiv::hill.div(abund,o)}
+            if(level == "gamma"){div.values <- hilldiv::gamma.div(abund,o)}
+            if(level == "alpha"){div.values <- hilldiv::alpha.div(abund,o)}
             }else{
-            div.values <- hilldiv::hill.div(abund,o,tree)
+            ltips <- sapply(tree$edge[, 2], function(node) geiger::tips(tree, node))
+            if(level == "NA"){div.values <- hill.div.fast(abund,o,tree)}
+            if(level == "gamma"){div.values <- gamma.div(abund,o,tree)}
+            if(level == "alpha"){div.values <- alpha.div(abund,o,tree)}
             }
         profile <- rbind(profile,div.values)
         }
-        rownames(profile) <- qvalues
-        profile.melted <- as.data.frame(melt(profile))
-        colnames(profile.melted) <- c("Order","Sample","Value")
-        profile.melted[,1] <- as.numeric(as.character(profile.melted[,1]))
-        profile.melted[,3] <- as.numeric(as.character(profile.melted[,3]))
-        if(log == "TRUE"){profile.melted[,3] <- log(profile.melted[,3])}
+        if(level == "NA"){
+          rownames(profile) <- qvalues
+          }
+        if(level == "gamma"){
+          profile <- c(profile)
+          names(profile) <- qvalues
+          }
+        if(level == "alpha"){
+          profile <- c(profile)
+          names(profile) <- qvalues
+          }
 
-        #Declare colours
-	if(missing(colour) || (length(colour) != ncol(abund))){
-        getPalette <- colorRampPalette(brewer.pal(ncol(abund), "Paired"))
-        colour <- getPalette(ncol(abund))
-        }
+    }
 
-        #Plot
-        plot <- ggplot(profile.melted , aes(x = Order, y = Value, group=Sample, colour=Sample)) +
-        geom_line() +
-        xlab("Order of diversity") +
-        ylab(if(log == "TRUE"){"Effective number of OTUs (log-transformed)" }else{"Effective number of OTUs"}) +
-        scale_colour_manual(values = colour) +
-        theme_minimal()
-        print(plot)
-
-    }else{
+    #With hierarchy
+    if(!missing(hierarchy)){
+    if(ncol(hierarchy) != 2) stop("The hierarchy table must contain two columns.")
     colnames(hierarchy) <- c("Sample","Group")
-    groups <- sort(unique(hierarchy$Group))
-
+    groups <- as.character(sort(unique(hierarchy$Group)))
+    profile <- c()
         for (g in groups){
             samples <- as.character(hierarchy[which(hierarchy$Group == g),1])
             abund.subset <- abund[,samples]
             abund.subset <- as.data.frame(abund.subset[apply(abund.subset, 1, function(z) !all(z==0)),])
 
-            if(!missing(tree)){                                       
+            #Subset tree
+            if(!missing(tree)){
             missing.otus <- setdiff(tree$tip.label,rownames(abund.subset))
             tree.subset <- drop.tip(tree,missing.otus)
             }
-                 for (o in qvalues){
-                        if(missing(tree)){ 
-                            if(level == "gamma"){div.value <- hilldiv::gamma.div(abund.subset,o)}
-                            if(level == "alpha"){div.value <- hilldiv::alpha.div(abund.subset,o)}
-                            if(level == "incidence"){div.value <- hilldiv::hill.div(rowSums(abund.subset != 0)/sum(rowSums(abund.subset != 0)),o)}
-                        }else{
-                            if(level == "gamma"){div.value <- hilldiv::gamma.div(abund.subset,o,tree.subset)}
-                            if(level == "alpha"){div.value <- hilldiv::alpha.div(abund.subset,o,tree.subset)}
-                            if(level == "incidence"){div.value <- hilldiv::hill.div(rowSums(abund.subset != 0)/sum(rowSums(abund.subset != 0)),o,tree.subset)}
+
+            for (o in qvalues){
+                  if(missing(tree)){
+                        if(level == "NA"){div.value <- hilldiv::gamma.div(abund.subset,o)}
+                        if(level == "gamma"){div.value <- hilldiv::gamma.div(abund.subset,o)}
+                        if(level == "alpha"){div.value <- hilldiv::alpha.div(abund.subset,o)}
+                  }else{
+                        ltips <- sapply(tree.subset$edge[, 2], function(node) geiger::tips(tree.subset, node))
+                        if(level == "NA"){div.value <- gamma.div.fast(abund.subset,o,tree.subset)}
+                        if(level == "gamma"){div.value <- gamma.div.fast(abund.subset,o,tree.subset)}
+                        if(level == "alpha"){div.value <- alpha.div.fast(abund.subset,o,tree.subset)}
                         }
-                 profile <- rbind(profile,cbind(g,div.value))
+            profile <- rbind(profile,as.numeric(div.value))
+            }
+      }
 
-                 }    
-          }
-                                                             
-    profile <- as.data.frame(cbind(profile,rep(qvalues,length(groups))))
-    profile[,2] <- as.numeric(as.character(profile[,2]))
-    profile[,3] <- as.numeric(as.character(profile[,3]))
-    colnames(profile) <- c("Group","Value","Order")
-    if(log == "TRUE"){profile[,2] <- log(profile[,2])}
-
-    #Declare colours
-    if(missing(colour) || (length(colour) != length(groups))){
-    getPalette <- colorRampPalette(brewer.pal(length(groups), "Paired"))
-    colour <- getPalette(length(groups))
-    }                                                             
-    
-    #Plot                                                         
-    plot <- ggplot(profile , aes(x = Order, y = Value, group=Group, colour=Group)) +
-    geom_line() + 
-    xlab("Order of diversity") + 
-    ylab(if((log == "TRUE") & missing(tree)){"Effective number of OTUs (log-transformed)"}else if((log == "TRUE") & !missing(tree)){"Effective number of lineages (log-transformed)"}else if((log == "FALSE") & !missing(tree)){"Effective number of lineages"}else{"Effective number of OTUs"}) +
-    scale_colour_manual(values = colour) + 
-    theme_minimal()
-    print(plot)
-                                                                                                                     
-    }
-                                                   
-    
-    if(values == "TRUE"){
-    return(profile)
-    }
-                                     
-    
+    profile <- matrix(profile,nrow=length(qvalues))
+    colnames(profile) <- as.character(groups)
+    rownames(profile) <- as.character(qvalues)
+  }
 }
-
+return(profile)
 }
---
+++ R/div.profile.plot.R
@@ -0,0 +1,76 @@
+#' Diversity profile plot
+#' @title Diversity profile plot
+#' @author Antton Alberdi, \email{anttonalberdi@gmail.com}
+#' @keywords alpha gamma beta hill
+#' @description Plot diversity profiles of a single or multiple samples displayed independently or aggregated in groups.
+#' @param profile A div.profile() object or a vector/matrix containg diversity profile(s), with columns indicating samples/groups and rows indicating orders of diversity (q-values).
+#' @param colour The number of vector items (colours, e.g. '#34k235'), must equal the number of samples or groups that are intended to plot.
+#' @param log If 'TRUE' the Hill numbers are transformed to the logarithmic scale. This is useful when there are large differences between q values (e.g. sharp drop from q=0 to q=1), which might complicate visualization. log="FALSE"
+#' @param legend Whether to display the legend (TRUE) or not (FALSE) in diversity profiles containing multiple samples/groups. Default TRUE in multi-sample charts.
+#' @return A diversity profile plot.
+#' @seealso \code{\link{div.profile}}, \code{\link{hill.div}}
+#' @examples
+#' div.profile.plot(profile)
+#' @references
+#' Alberdi, A., Gilbert, M.T.P. (2019). A guide to the application of Hill numbers to DNA-based diversity analyses. Molecular Ecology Resources. Early view.\cr\cr
+#' Chao, A., Chiu, C.‐H., & Jost, L. (2014). Unifying species diversity, phylo‐ genetic diversity, functional diversity, and related similarity and dif‐ ferentiation measures through hill numbers. Annual Review of Ecology Evolution and Systematics, 45, 297–324.
+#' @export
+
+div.profile.plot <- function(profile,colour,log,legend){
+
+#Data check
+if(missing(profile)) stop("The diversity profile vector/table is missing")
+if(is.null(dim(profile)) == TRUE){
+  inputtype="onesample"
+}else{
+  inputtype="multiplesamples"
+  }
+if(missing(log)){log == "FALSE"}
+if(missing(legend)){legend == "TRUE"}
+
+#Declare colours
+if(missing(colour) || (length(colour) != ncol(profile))){
+colour <- colorRampPalette(brewer.pal(12, "Paired"))(ncol(profile))
+}
+
+#Single sample/group
+if(inputtype == "onesample"){
+  if(length(colour != 1)){colour="#99cc00"}
+  profile.melted <- as.data.frame(cbind(names(profile),profile))
+  colnames(profile.melted) <- c("Order","Profile")
+  profile.melted[,1] <- as.numeric(as.character(profile.melted[,1]))
+  profile.melted[,2] <- as.numeric(as.character(profile.melted[,2]))
+  if(log == "TRUE"){profile.melted[,2] <- log(profile.melted[,2])}
+
+  plot <- ggplot(profile.melted , aes(x = Order, y = Profile, col=colour)) +
+         geom_line() +
+         xlab("Order of diversity") +
+         ylab(if(log == "TRUE"){"Effective number of OTUs (log-transformed)" }else{"Effective number of OTUs"}) +
+         theme_minimal() +
+         theme(legend.position = "none")
+  print(plot)
+}
+
+#Multiple samples/groups
+if(inputtype == "multiplesamples"){
+  profile.melted <- as.data.frame(melt(profile))
+  colnames(profile.melted) <- c("Order","Sample","Value")
+  profile.melted[,1] <- as.numeric(as.character(profile.melted[,1]))
+  profile.melted[,3] <- as.numeric(as.character(profile.melted[,3]))
+  if(log == "TRUE"){profile.melted[,3] <- log(profile.melted[,3])}
+
+  #Plot
+  plot <- ggplot(profile.melted , aes(x = Order, y = Value, group=Sample, colour=Sample)) +
+  geom_line() +
+  xlab("Order of diversity") +
+  ylab(if(log == "TRUE"){"Effective number of OTUs (log-transformed)" }else{"Effective number of OTUs"}) +
+  scale_colour_manual(values = colour) +
+  theme_minimal()
+  if(legend == "TRUE"){
+  plot <- plot + theme(legend.position = "right")}
+  if(legend == "FALSE"){
+  plot <- plot + theme(legend.position = "none")}
+  print(plot)
+}
+
+}
